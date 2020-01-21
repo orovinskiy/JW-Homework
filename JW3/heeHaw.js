8800ecf881ff8fd6e65a0a84c73c7d46ae31e0b2
Oleg Rovinskiy <orovinskiy@mail.greenriver.edu>
2020-01-21 17:53:10 -0500

    Put hee haw program into a recursive function only called if the user provides the correct output

---
+++ JW3/heeHaw.js
@@ -0,0 +1,39 @@
+let valid = false;
+let number;
+while(!valid){
+    number = parseInt(prompt("Enter a positive number that's under 500"));
+    if(isNaN(number)){
+        alert("Only positive numbers are allowed.");
+    }
+    else if(number <= 0){
+        alert("Number cant be a negative or zero");
+    }
+    else if(number > 500){
+        alert("Number cant be higher then 500");
+    }
+    else{
+       valid = true;
+    }
+}
+
+if(valid){
+    recurse(number);
+}
+
+function recurse(number){
+    if(number > 0){
+        recurse(number-1);
+        if(number % 3 === 0 && number  % 5 === 0){
+            console.log("Hee Haw!");
+        }
+        else if(number  % 5 === 0){
+            console.log("Haw!");
+        }
+        else if(number  % 3 === 0){
+            console.log("Hee!");
+        }
+        else{
+            console.log(number);
+        }
+    }
+}
